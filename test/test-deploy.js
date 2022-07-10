8f6524e6acf4dd65ba334cd9a1bef353a8c65e9e
ritesh gurjar <gurjarritesh155@gmail.com>
2022-07-10 16:55:43 +0530

    first commit

---
+++ test/test-deploy.js
@@ -0,0 +1,26 @@
+const { inputToConfig } = require("@ethereum-waffle/compiler")
+const { expect, assert } = require("chai")
+const { ethers } = require("hardhat")
+
+describe("SimpleStorage", function () {
+    let simpleStorageFactory, simpleStorage
+    beforeEach(async function () {
+        simpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
+        simpleStorage = await simpleStorageFactory.deploy()
+    })
+
+    it("Should stsrt with a favourite number of 0 ", async function () {
+        const currentValue = await simpleStorage.retrieve()
+        const expectedValue = "0"
+
+        assert.equal(currentValue.toString(), expectedValue)
+    })
+    it("Should update when we call store", async function () {
+        const expectedValue = "7"
+        const transactionResponse = await simpleStorage.store(expectedValue)
+        await transactionResponse.wait(1)
+
+        const currentValue = await simpleStorage.retrieve()
+        assert.equal(currentValue.toString(), expectedValue)
+    })
+})
